958702f73bcf4941c2dbf436a498a3e62732b947
pranjalsingh7456 <pranjalsingh1811@gmail.com>
2026-05-02 01:33:29 +0530

    happy_number

---
+++ happy_number.py
@@ -0,0 +1,12 @@
+
+def happy(n):
+    for _ in range(100):  # limit iterations
+        sum_sq = 0
+        while n > 0:
+            digit = n % 10
+            sum_sq += digit * digit
+            n //= 10
+        n = sum_sq
+    return n == 1
+
+print(happy(19))
--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def twoSum(self, nums, target):
         seen = {}
